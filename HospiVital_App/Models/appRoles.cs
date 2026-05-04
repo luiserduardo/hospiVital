58faaa64124cfda5904424ef5ee38cc121b6ee97
Yanira Rivas <rivasgomezclaudiayanira@gmail.com>
2026-05-03 20:01:45 -0600

    Delete HospiVital_App directory

--- HospiVital_App/Models/appRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HospiVital_App.Models
-{
-    public static class AppRoles
-    {
-        public const string AsistenteMedico = "AsistenteMedico";
-        public const string Medico = "Medico";
-        public const string Admin = "Admin";
-    }
-}
